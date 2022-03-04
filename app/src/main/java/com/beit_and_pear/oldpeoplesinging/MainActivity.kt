9af2d55ab43d9ca4a57930e2dd6281d4d9954967
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-03-04 21:34:20 +0900

    MainActivity.ktを修正

--- app/src/main/java/com/beit_and_pear/oldpeoplesinging/MainActivity.kt
+++ app/src/main/java/com/beit_and_pear/oldpeoplesinging/MainActivity.kt
@@ -2,6 +2,8 @@ package com.beit_and_pear.oldpeoplesinging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,20 @@ class MainActivity : AppCompatActivity() {
         // タイトルバーを非表示
         supportActionBar?.hide()
 
+        // ページで利用するデータを準備
+        val data = listOf(
+            ListItem(
+                title = "一月",
+                photo = ContextCompat.getDrawable(this, R.drawable.takibi_1)
+            ),
+            ListItem(
+                title = "一月",
+                photo = ContextCompat.getDrawable(this, R.drawable.takibi_1)
+            ),
+        )
 
+        // ViewPager2にデータをバインド
+        val pager = findViewById<ViewPager2>(R.id.pager)
+        pager.adapter = MyListAdapter(data)
     }
 }
